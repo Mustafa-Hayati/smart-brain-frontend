a3fd41a845fa150081e2ff3abc1f39a8d64a3d3c
Mustafa-Hayati <mustafa.hayati1992@gmail.com>
2019-11-30 16:07:50 +0330

    created all the components of homepage, without adding logic to them

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rank = () => (
+  <div>
+    <div className="white f3">
+      {"Mustafa your current rank is..."}
+    </div>
+    <div className="white f1">{"#13"}</div>
+  </div>
+);
+
+export default Rank;
